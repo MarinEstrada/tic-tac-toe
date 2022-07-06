4191aef74201979bf147b84f623c9d042d930490
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-06 16:34:21 -0700

    DOCS: convert on click to arrow function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css'
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function() {
-                console.log('click'); }} >
+            <button className="square" onClick={() =>
+                console.log('click')} >
                 {this.props.value}
             </button>
         );
