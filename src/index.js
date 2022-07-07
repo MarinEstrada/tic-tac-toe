dcd97db4b5d16c8cffe182e760f6f8504687ed34
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-06 20:49:21 -0700

    WIP: edit squares class to handle props instead of state
    state of game now handeled by board class. Squares now handles props
    passed down from board

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css'
 
 class Square extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            value:null,
-        };
-    }
-
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({value: 'X'})}
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
