54a914660b29063152686e65ebfccc9e5a87f98c
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-07 19:50:38 -0700

    WIP: add key for prevMoves button, and implement jumpTo

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
@@ -74,6 +75,13 @@ class Game extends React.Component {
 
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
 
         const history = this.state.history;
@@ -85,7 +93,7 @@ class Game extends React.Component {
                 "Go to move #" + move :
                 "go to game start";
             return(
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)} >
                         {desc}
                     </button>
