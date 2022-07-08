d89a05efc70a3a0fd0102cb1c3fd2db88a843b0d
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-07 20:02:12 -0700

    FINISH: have board render state based off stepNumber

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber +1);
         const current =  history[history.length -1];
         const squares = current.squares.slice();
 
@@ -70,6 +70,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
 
@@ -85,7 +86,7 @@ class Game extends React.Component {
     render() {
 
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
